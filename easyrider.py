7d86e105553c0047f47d62472e91e6e4a2814670
sheff-slava <sheff-slava@mail.ru>
2021-04-19 11:51:27 +0300

    Step 5

--- easyrider.py
+++ easyrider.py
@@ -1,6 +1,24 @@
 import json
 
 
+def check_time(time_1, time_2):
+    h_1, m_1 = time_1.split(':')
+    h_2, m_2 = time_2.split(':')
+    h_1 = int(h_1[1]) if h_1.startswith('0') else int(h_1)
+    h_2 = int(h_2[1]) if h_2.startswith('0') else int(h_2)
+    m_1 = int(m_1[1]) if m_1.startswith('0') else int(m_1)
+    m_2 = int(m_2[1]) if m_2.startswith('0') else int(m_2)
+    if h_1 < h_2:
+        return True
+    elif h_1 > h_2:
+        return False
+    else:
+        if m_1 < m_2:
+            return True
+        elif m_1 >= m_2:
+            return False
+
+
 json_obj1 = '''[
     {
         "bus_id": 128,
@@ -24,7 +42,7 @@ json_obj1 = '''[
         "stop_name": "Fifth Avenue",
         "next_stop": 7,
         "stop_type": "O",
-        "a_time": "08:25"
+        "a_time": "08:17"
     },
     {
         "bus_id": 128,
@@ -32,58 +50,7 @@ json_obj1 = '''[
         "stop_name": "Sesame Street",
         "next_stop": 0,
         "stop_type": "F",
-        "a_time": "08:37"
-    },
-    {
-        "bus_id": 512,
-        "stop_id": 4,
-        "stop_name": "Bourbon Street",
-        "next_stop": 6,
-        "stop_type": "",
-        "a_time": "08:13"
-    },
-    {
-        "bus_id": 512,
-        "stop_id": 6,
-        "stop_name": "Sunset Boulevard",
-        "next_stop": 0,
-        "stop_type": "F",
-        "a_time": "08:16"
-    }
-]'''
-
-json_obj2 = '''[
-    {
-        "bus_id": 128,
-        "stop_id": 1,
-        "stop_name": "Prospekt Avenue",
-        "next_stop": 3,
-        "stop_type": "S",
-        "a_time": "08:12"
-    },
-    {
-        "bus_id": 128,
-        "stop_id": 3,
-        "stop_name": "Elm Street",
-        "next_stop": 5,
-        "stop_type": "",
-        "a_time": "08:19"
-    },
-    {
-        "bus_id": 128,
-        "stop_id": 5,
-        "stop_name": "Fifth Avenue",
-        "next_stop": 7,
-        "stop_type": "O",
-        "a_time": "08:25"
-    },
-    {
-        "bus_id": 128,
-        "stop_id": 7,
-        "stop_name": "Sesame Street",
-        "next_stop": 0,
-        "stop_type": "F",
-        "a_time": "08:37"
+        "a_time": "08:07"
     },
     {
         "bus_id": 256,
@@ -107,7 +74,7 @@ json_obj2 = '''[
         "stop_name": "Sunset Boulevard",
         "next_stop": 7,
         "stop_type": "",
-        "a_time": "09:59"
+        "a_time": "09:44"
     },
     {
         "bus_id": 256,
@@ -135,55 +102,46 @@ json_obj2 = '''[
     }
 ]'''
 
+json_obj2 = '''[
+    {
+        "bus_id": 512,
+        "stop_id": 4,
+        "stop_name": "Bourbon Street",
+        "next_stop": 6,
+        "stop_type": "S",
+        "a_time": "08:13"
+    },
+    {
+        "bus_id": 512,
+        "stop_id": 6,
+        "stop_name": "Sunset Boulevard",
+        "next_stop": 0,
+        "stop_type": "F",
+        "a_time": "08:16"
+    }
+]'''
 
-list_of_dict = list(json.loads(input()))
-# list_of_dict = list(json.loads(json_obj2))
-list_of_dict.sort(key=lambda x: x['bus_id'])
 
-start_set = set()
-finish_set = set()
-stops_by_lines = dict()
+list_of_dict = json.loads(input())
+# list_of_dict = json.loads(json_obj1)
+# list_of_dict.sort(key=lambda x: x['bus_id'])
 
+wrong_time_dict = dict()
 bus_id = list_of_dict[0]['bus_id']
-start_count = 0
-finish_count = 0
-no_start_finish = False
+found_wrong_time = False
 i = 0
-while i < len(list_of_dict):
-    while list_of_dict[i]['bus_id'] == bus_id:
-        if list_of_dict[i]['stop_type'] == 'S':
-            start_count += 1
-            start_set.update({list_of_dict[i]['stop_name']})
-        elif list_of_dict[i]['stop_type'] == 'F':
-            finish_count += 1
-            finish_set.update({list_of_dict[i]['stop_name']})
-        if list_of_dict[i]['stop_name'] not in stops_by_lines.keys():
-            list_ = list()
-            list_.append(bus_id)
-            stops_by_lines[list_of_dict[i]['stop_name']] = list_
-        else:
-            list_ = stops_by_lines[list_of_dict[i]['stop_name']]
-            list_.append(bus_id)
-            stops_by_lines[list_of_dict[i]['stop_name']] = list_
-        i += 1
-        if i == len(list_of_dict):
-            break
-    if start_count != 1 or finish_count != 1:
-        print(f'There is no start or end stop for the line: {bus_id}.')
-        no_start_finish = True
-        break
-    elif i < len(list_of_dict):
-        bus_id = list_of_dict[i]['bus_id']
-        start_count = 0
-        finish_count = 0
+for i in range(len(list_of_dict) - 1):
+    if not found_wrong_time and not check_time(list_of_dict[i]['a_time'], list_of_dict[i + 1]['a_time'])\
+            and list_of_dict[i]['bus_id'] == list_of_dict[i + 1]['bus_id']:
+        wrong_time_dict[bus_id] = list_of_dict[i + 1]['stop_name']
+        found_wrong_time = True
+    elif list_of_dict[i]['bus_id'] != list_of_dict[i + 1]['bus_id']:
+        bus_id = list_of_dict[i + 1]['bus_id']
+        found_wrong_time = False
 
-if not no_start_finish:
-    start_stops = list(start_set)
-    transfer_stops = list()
-    finish_stops = list(finish_set)
-    for stop, list_id in stops_by_lines.items():
-        if len(list_id) > 1:
-            transfer_stops.append(stop)
-    print('Start stops:', len(start_stops), sorted(start_stops))
-    print('Transfer stops:', len(transfer_stops), sorted(transfer_stops))
-    print('Finish stops:', len(finish_stops), sorted(finish_stops))
+print('Arrival time test:')
+if len(wrong_time_dict) == 0:
+    print('OK')
+else:
+    for line, station in wrong_time_dict.items():
+        print(f'bus_id line {line}: wrong time on station {station}')
